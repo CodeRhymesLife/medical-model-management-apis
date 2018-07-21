ff7509f29fa3d933af36ad68bcd64d6f2a306972
Ryan James <rcjames1004@gmail.com>
2018-07-21 12:57:15 -0700

    Remove dependencies in users tests. Bug #4

--- src/routes/users/users.test.ts
+++ src/routes/users/users.test.ts
@@ -4,69 +4,110 @@ import chai from 'chai';
 
 import app from '../../index';
 import settings from '../../config/settings';
-import { testData } from '../../tests/testUtils';
+import { createUser, testData, TestUser } from '../../tests/testUtils';
+import { UserModel } from './users.model';
 
 const expect = chai.expect;
 chai.config.includeStack = true;
 
 describe('## User APIs', () => {
-    // User created from id token
-    let createdUser: any  = undefined;
+    /** Validates the response retruend from one of the /users APIs */
+    const validateUserResponse = (responseObj: any, expected: TestUser): void => {
+        expect(responseObj.body.name).to.equal(expected.auth.name);
+        expect(responseObj.body.email).to.equal(expected.auth.email);
+    };
+
+    /** Remove all users after each test */
+    afterEach(() => {
+        return UserModel.remove({});
+    });
 
     describe('# POST /users', async () => {
+        // Create a new user
         it('should create a new user', async () => {
             const res = await request(app)
                 .post('/users')
                 .set(settings.headers.idToken, testData.users.one.idToken)
                 .expect(httpStatus.CREATED);
 
-            expect(res.body.name).to.equal(testData.users.one.auth.name);
-            expect(res.body.email).to.equal(testData.users.one.auth.email);
-            createdUser = res.body;
-        });
+            // Validate the reponse
+            validateUserResponse(res.body, testData.users.one);
+         });
     });
 
-    describe('# GET /users/:userId', () => {
-        it('should get user details', async () => {
+    describe('# GET /users/:userId', async () => {
+        // Create the user
+        const user = await createUser(testData.users.one);
+
+        it('should get user details for self', async () => {
+            // Get the user's details
             const res = await request(app)
-                .get(`/users/${createdUser._id}`)
+                .get(`/users/${user._id}`)
                 .set(settings.headers.idToken, testData.users.one.idToken)
                 .expect(httpStatus.OK);
 
-            expect(res.body.name).to.equal(createdUser.name);
-            expect(res.body.email).to.equal(createdUser.email);
+            // Validate the reponse
+            validateUserResponse(res.body, testData.users.one);
+        });
+
+        it('should get user details for another user', async () => {
+            // Create user one
+            await createUser(testData.users.one);
+
+            // Create second user
+            const userTwo = await createUser(testData.users.two);
+
+            // Get the user two's details
+            const res = await request(app)
+                .get(`/users/${userTwo._id}`)                               // Get user two's details
+                .set(settings.headers.idToken, testData.users.one.idToken)  // with user one's creds
+                .expect(httpStatus.OK);
+
+            // Validate the reponse
+            validateUserResponse(res.body, testData.users.two);
         });
 
         it('should report error with message - Not found, when user does not exists', async () => {
+            // Attempt to get a user that doesn't exist
             const res = await request(app)
-                .get('/users/56c787ccc67fc16ccc1a5e92')
+                .get(`/users/${testData.invalidId}`)
                 .set(settings.headers.idToken, testData.users.one.idToken)
                 .expect(httpStatus.NOT_FOUND);
 
+            // Validate the response
             expect(res.body.message).to.equal('Not Found');
         });
-    });
+   });
 
-    describe('# DELETE /users/', () => {
+    describe('# DELETE /users/', async () => {
         it('should report error with message - Unauthorized - when a non-master user tries to delete', async () => {
+            // Create the user
+            const user = await createUser(testData.users.one);
+
+            // Attempt to delete the user with a non-master user
             const res = await request(app)
-                .delete(`/users/${createdUser._id}`)
+                .delete(`/users/${user._id}`)
                 .set(settings.headers.idToken, testData.users.one.idToken)
-            // .set(settings.headers.testIsMaster, true) // The testIsMaster request header is not set
+                // .set(settings.headers.testIsMaster, true) // The testIsMaster request header is not set
                 .expect(httpStatus.UNAUTHORIZED);
 
+            // Validate the response message
             expect(res.body.message).to.equal('Unauthorized');
         });
 
         it('should delete user', async () => {
+            // Create the user
+            const user  = await createUser(testData.users.one);
+
+            // Delete the user
             const res = await request(app)
-                .delete(`/users/${createdUser._id}`)
+                .delete(`/users/${user._id}`)
                 .set(settings.headers.idToken, testData.users.one.idToken)
                 .set(settings.headers.testIsMaster, 'true')
                 .expect(httpStatus.OK);
 
-            expect(res.body.name).to.equal(createdUser.name);
-            expect(res.body.email).to.equal(createdUser.email);
-        });
+            // Validate the reponse
+            validateUserResponse(res.body, testData.users.one);
+         });
     });
 });
