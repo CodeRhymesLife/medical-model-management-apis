dd1019a7e543880b09c421ceec794e493a28a944
Ryan James <rcjames1004@gmail.com>
2018-10-23 03:53:03 -0700

    Run js and ts on docker

--- src/index.routes.ts
+++ src/index.routes.ts
@@ -6,7 +6,7 @@ import { logger } from './config/winston';
 
 const LOG_TAG = '[routes]';
 
-const router = express.Router(); // eslint-disable-line new-cap
+const router = express.Router();
 
 /**
  * Mounts all routes defined in *.route.js files in routes/
@@ -22,15 +22,17 @@ const mountRoutes = () => {
     // Mount routes for each file
     files.forEach((routeFilename: string) => {
         const relativePath = path.relative(__dirname, routeFilename);
-        const routes = require(`./${relativePath}`); // eslint-disable-line global-require
+        const routes = require(`./${relativePath}`);
 
         // Create the url using the first part of the filename
         // e.g. auth.route.js will generate /auth
-        const routeName = path.basename(routeFilename, '.route.js');
+        const routeBaseName = path.basename(routeFilename);
+        const extensionIndex = routeBaseName.indexOf('.route.');
+        const routeName = routeBaseName.substring(0, extensionIndex);
         const url = `/${routeName}`;
 
         // Mount the routes
-        logger.info(`${LOG_TAG} ${path.basename(routeFilename)} -> ${url}`); // eslint-disable-line no-console
+        logger.info(`${LOG_TAG} ${routeBaseName} -> ${url}`);
         router.use(url, routes.default); // https://stackoverflow.com/questions/40294870/module-exports-vs-export-default-in-node-js-and-es6
     });
 };
